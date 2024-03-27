67db59701d4179c16c6a4333f54ca43a5fe54594
Varits106 <106133224+Varits106@users.noreply.github.com>
2024-03-27 16:28:54 +0700

    first commit

---
+++ jenna-react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="m-0 text-center">
+          © 2023 copyright all right reserved | Varits Gorga
+        </p>
+      </div>
+    </footer>
+  );
+}
